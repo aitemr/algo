4b6306db85f34a5b1819dd03af4ee9cd0743e3f6
Islam Temirbek <aitemr16@gmail.com>
2018-11-03 22:51:32 +0600

    leetcode: add 136 - Single Number

---
+++ leetcode/136 - Single Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            result ^= nums[i];
+        }
+
+        return result;     
+    }
+};
