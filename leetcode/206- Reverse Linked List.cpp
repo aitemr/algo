1121db45ab5cd87f3d47040a7390f088286026f8
Islam Temirbek <aitemr16@gmail.com>
2018-11-05 23:06:45 +0600

    leetcode: add 206- Reverse Linked List

---
+++ leetcode/206- Reverse Linked List.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+        
+        return head;
+    }
+}
