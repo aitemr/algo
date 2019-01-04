a8ccd62d910e0da99241d554eff4a9ff9f564a8f
Islam Temirbek <aitemr16@gmail.com>
2019-01-04 06:36:06 +0600

    leetcode: add 1 - Two Sum

---
+++ leetcode/1 - Two Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> indices;  
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    indices.push_back(i);
+                    indices.push_back(j);
+                }
+            }
+        }
+
+        return indices;
+    }
+};
