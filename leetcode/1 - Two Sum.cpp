4c50abe9d09e676646cd8e607036963f6c2f2f1b
Islam Temirbek <aitemr16@gmail.com>
2019-01-04 06:42:51 +0600

    fix: fix code style

--- leetcode/1 - Two Sum.cpp
+++ leetcode/1 - Two Sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> indices;  
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int i = 0; i < nums.size(); i++) {
             for (int j = i+1; j < nums.size(); j++) {
                 if (nums[i] + nums[j] == target) {
                     indices.push_back(i);
