cb85437546946b8093cbbb5558dca4248af9ab50
Islam Temirbek <aitemr16@gmail.com>
2019-01-06 21:52:32 +0600

    leetcode: improve solution 7 - Reverse Integer

--- leetcode/7 - Reverse Integer.cpp
+++ leetcode/7 - Reverse Integer.cpp
@@ -1,39 +1,16 @@
-#include <stdio.h>
-#include <iostream>
-#include <cstring>
-#include <vector>
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if ((rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) || 
+                (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8))) return 0;
 
-using namespace std;
-
-int convertString(const char *s) {
-    int i = 0;
-    while(*s >= '0' && *s <= '9') {
-        i = i * 10 + (*s - '0');
-        s++;
-    }
-
-    return i;
-}
-
-int reverse(int x) {
-    string str = to_string(x > 0 ? x : -x);
-    string reversedString = "";
-
-
-    cout << "str " << str << endl;
-
-    for (int i = str.length() - 1; i >= 0;  i--) {
-        cout << "str[i] " << str[i] << endl;
-        reversedString += str[i];
-    }
-
-    cout << "reversedString " << reversedString << endl;
-
-    return x > 0 ? stol(reversedString) : -stol(reversedString);
-}
-
-int main() {
-    cout << reverse(-16700900);
-
-    return 0;
-}
+            rev = rev * 10 + pop;
+        }
+    
+        return rev;
+    }    
+};
