7f7f91769c888442313f53dcc532447081fb1b12
Islam Temirbek <aitemr16@gmail.com>
2019-01-08 06:03:25 +0600

    leetcode: improve solution 9 - Palindrome Number

--- leetcode/9 - Palindrome Number.cpp
+++ leetcode/9 - Palindrome Number.cpp
@@ -24,8 +24,23 @@ bool isPalindrome(int x) {
         return true;
 }
 
+bool isPalindromeWithoutConverting(int x) {
+
+    if (x < 0 || ( x == 0 && x % 10 == 0)) {
+        return false;
+    }
+
+    int revertedNumber = 0;
+    while(x > revertedNumber) {
+        revertedNumber = revertedNumber * 10 + x % 10;
+        x /= 10;
+   }
+
+    return x == revertedNumber || x == revertedNumber/10;
+}
+
 int main() {
-    cout << isPalindrome(12221) << endl;
+    cout << isPalindromeWithoutConverting(12321) << endl;
 
     return 0;
 }
