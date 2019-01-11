5df751258327caa0e4c557f1c09222b0bfc7f982
Islam Temirbek <aitemr16@gmail.com>
2019-01-11 06:29:19 +0600

    leetcode: fix error 27 - Remove Element

--- leetcode/27 - Remove Element.cpp
+++ leetcode/27 - Remove Element.cpp
@@ -3,7 +3,7 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int j = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == val) {
+            if (nums[i] != val) {
                 nums[j++] = nums[i];
             }
         }
