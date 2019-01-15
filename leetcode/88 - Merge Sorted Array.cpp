7f47eb00f8b8ba1289d33cefb53e4f4c106079b8
Islam Temirbek <aitemr16@gmail.com>
2019-01-15 23:08:21 +0600

    leetcode: add 88 - Merge Sorted Array

---
+++ leetcode/88 - Merge Sorted Array.cpp
@@ -0,0 +1,34 @@
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+// Note:
+
+// The number of elements initialized in nums1 and nums2 are m and n respectively.
+// You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+// Example:
+
+// Input:
+// nums1 = [1,2,3,0,0,0], m = 3
+// nums2 = [2,5,6],       n = 3
+
+// Output: [1,2,2,3,5,6]
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int a = m - 1;
+        int b = n - 1;
+        int c = m + n - 1;
+        
+        while (b >= 0 && a >= 0) {
+            if (nums1[a] >= nums2[b]) {
+                nums1[c--] = nums1[a--];
+            } else {
+                nums1[c--] = nums2[b--];
+            }
+        }
+
+        while (b >= 0){
+            nums1[c--] = nums2[b--];
+        }
+    }
+};
