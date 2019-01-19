4ea4e791a41d21445b83942bf62f3099f458ae05
Islam Temirbek <aitemr16@gmail.com>
2019-01-19 22:34:30 +0600

    leetcode: update 118 - Pascals Triangle

--- leetcode/118 - Pascal's Triangle.cpp
+++ leetcode/118 - Pascal's Triangle.cpp
@@ -1,39 +1,27 @@
 // Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
 // In Pascal's triangle, each number is the sum of the two numbers directly above it.
 
-#include <iostream>
-#include <vector>
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int> > r;
+        if (numRows <= 0) {
+            return r;
+        }
 
-using namespace std;
-
-vector<vector<int> > generate(int numRows) {
-    vector<vector<int> > r;
-    if (numRows <= 0) {
-        return r;
-    }
-
-    vector<int> cur;
-    cur.push_back(1);
-    r.push_back(cur);
-
-    for (int i = 0; i < numRows - 1; i ++) {
-        for (int j = cur.size() - 1; j > 0; j--) {
-            cur[j] += cur[j - 1];
-
-            cout << cur[j] << endl;
-        }   
-
+        vector<int> cur;
         cur.push_back(1);
-
         r.push_back(cur);
+
+        for (int i = 0; i < numRows - 1; i++) {
+            for (int j = cur.size() - 1; j > 0; j--) {
+                cur[j] += cur[j - 1];
+            }   
+
+            cur.push_back(1);
+            r.push_back(cur);
+        }
+
+        return r;  
     }
-
-    return r;
-}
-
-int main() {
-    generate(5);
-
-    return 0;
-}
-
+};
