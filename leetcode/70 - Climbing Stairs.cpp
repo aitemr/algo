5efdf60084b0a4213ff5a22071b235613a7993e6
Islam Temirbek <aitemr16@gmail.com>
2019-01-20 16:31:11 +0600

    leetcode: add 70 - Climbing Stairs

---
+++ leetcode/70 - Climbing Stairs.cpp
@@ -0,0 +1,51 @@
+// You are climbing a stair case. It takes n steps to reach to the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Note: Given n will be a positive integer.
+
+// Example 1:
+// Input: 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+// Example 2:
+// Input: 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+#include <iostream>
+
+using namespace std;
+
+int climbStairs(int n) {
+
+    if (n == 1) {
+        return 1;
+    }
+
+    int first = 1;
+    int second = 2;
+    
+    for (int i = 3; i <= n; i++) {
+        int third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return second;
+} 
+
+
+int main() {
+
+    climbStairs(10);
+
+
+    return 0;
+}
