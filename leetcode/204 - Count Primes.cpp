2ead7b7736e9707fb87481b139e646cbe4096ece
Islam Temirbek <aitemr16@gmail.com>
2019-01-27 17:17:32 +0600

    leetcode: update solution 204 - Count Primes

--- leetcode/204 - Count Primes.cpp
+++ leetcode/204 - Count Primes.cpp
@@ -6,32 +6,24 @@
 // Output: 4
 // Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 
-#include <iostream>
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            bool prime = true;
+            for (int j = 2; j*j <= i; j++) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
 
-using namespace std;
-
-int countPrimes(int n) {
-    int count = 0;
-    for (int i = 2; i < n; i++) {
-        bool prime = true;
-        for (int j = 2; j*j <= i; j++) {
-            if (i % j == 0) {
-                prime = false;
-                break;
+            if (prime) {
+                count++;
             }
         }
 
-        if (prime) {
-            count++;
-        }
+        return count; 
     }
-
-    return count;
-}
-
-int main() {
-
-    cout << countPrimes(10) << endl;
-
-    return 0;
-}
+};
